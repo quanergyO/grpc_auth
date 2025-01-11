035687e2d4df6abaf472874fba75cf55374a2173
pagemois <pagemois@student.21-school.ru>
2025-01-11 08:43:36 +0300

    implement logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/quaneryo/grpc_auth/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO init logger
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.Any("config", cfg))
 
 	// TODO init app
 
 	// TODO run gRPC-server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
