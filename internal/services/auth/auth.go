04dc42d8c2a0d6e68b0994cde7c3f4530878e054
pagemois <pagemois@student.21-school.ru>
2025-01-11 10:20:13 +0300

    implement base interface for sercice

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/quaneryo/grpc_auth/internal/domain/models"
+)
+
+type Auth struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+	tokenTTL     time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+// New returns a new instance of the Auth service.
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:          log,
+		userSaver:    userSaver,
+		userProvider: userProvider,
+		appProvider:  appProvider,
+		tokenTTL:     tokenTTL,
+	}
+}
+
+func (a *Auth) Login(
+	ctx context.Context,
+	email string,
+	password string,
+	appID int,
+) (string, error) {
+	panic("not implemented")
+}
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, password string) (userID int64, err error) {
+	panic("not implemented")
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	panic("not implemented")
+}
